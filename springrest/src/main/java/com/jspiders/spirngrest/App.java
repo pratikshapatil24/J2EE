3cc9328d7fa952ff17261aa45fbbc79f65b34e1c
Pratiksha Patil <130764415+pratikshapatil24@users.noreply.github.com>
2023-06-28 00:13:46 +0530

    Add files via upload

---
+++ springrest/src/main/java/com/jspiders/spirngrest/App.java
@@ -0,0 +1,5 @@
+package com.jspiders.spirngrest;
+
+public class App {
+
+}
---
+++ springrest/src/main/java/com/jspiders/spirngrest/response/StudentResponse.java
@@ -0,0 +1,21 @@
+package com.jspiders.spirngrest.response;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.jspiders.spirngrest.pojo.StudentPOJO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentResponse {
+	private String message;
+	private StudentPOJO data;
+	private List<StudentPOJO> list;
+
+}
